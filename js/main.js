bb0dbce2bb278a948f123a635505c45784955f4b
0sin <69961808+0sin@users.noreply.github.com>
2021-01-20 12:39:14 +0900

    Add main section slide, pagination

--- js/main.js
+++ js/main.js
@@ -31,11 +31,13 @@ function loop() {
 
 
 
-// SLIDE SHOW
+// MAIN SECTION SLIDE
 const sections = document.querySelectorAll('.main_section');
-let mainIndex = 0;
+let mainIndex = -1;
 
-function goToMain(idx) {
+const scrollToMoveAni = document.querySelector('.scroll_to_move');
+
+function changeMainIdx(idx) {
   mainIndex += idx;
 
   if (mainIndex > sections.length - 1) {
@@ -44,42 +46,111 @@ function goToMain(idx) {
     mainIndex = sections.length - 1;
   }
 
-  for (let i=0; i<sections.length; i++) {
-    sections[i].style.transform = "translate(0px, -100%)";
-  }
-  sections[mainIndex].style.transform = `translate(0px, 0px)`;
-  console.log(mainIndex);
+  pagination(mainIndex);
 }
-goToMain(mainIndex);
+
+nextMain(); //첫화면 호출
+
+function nextMain() {
+  changeMainIdx(1);
+
+  for (let i=0; i<sections.length; i++) {
+    sections[i].style.transform = "translate(0px, 100%)";
+    sections[i].style.zIndex = `1`;
+  }
+
+  if (mainIndex === sections.length - 1) { //마지막 페이지 예외처리
+    for (let i=0; i<sections.length; i++) {
+      sections[i].style.transform = "translate(0px, -100%)";
+      sections[i].style.zIndex = `1`;
+    }
+    sections[0].style.transform = "translate(0px, 100%)";
+    sections[0].style.zIndex = `2`;
+  } else if (mainIndex === 0) { //마지막 페이지 예외처리
+    sections[sections.length - 1].style.transform = `translate(0px, -100%)`;
+    sections[sections.length - 1].style.zIndex = `2`;
+  } else { //중간 페이지 적용
+    sections[mainIndex - 1].style.transform = "translate(0px, -100%)";
+    sections[mainIndex - 1].style.zIndex = `2`;
+  } 
+
+  sections[mainIndex].style.transform = `translate(0px, 0px)`;
+  sections[mainIndex].style.zIndex = `5`;
+
+}
 
 
+function prevMain() {
+  changeMainIdx(-1);
+
+  if (mainIndex === 0) {
+    for (let i=0; i<sections.length; i++) {
+      sections[i].style.transform = "translate(0px, 100%)";
+      sections[i].style.zIndex = `2`;
+    }
+    sections[sections.length - 1].style.transform = `translate(0px, -100%)`;
+    sections[sections.length - 1].style.zIndex = `1`;
+  } else if (mainIndex === sections.length - 1) {
+    for (let i=0; i<sections.length; i++) {
+      sections[i].style.transform = "translate(0px, -100%)";
+      sections[i].style.zIndex = `1`;
+    }
+    sections[0].style.transform = "translate(0px, 100%)";
+    sections[0].style.zIndex = `2`;
+  } else {
+    for (let i=0; i<sections.length; i++) {
+      sections[i].style.transform = "translate(0px, 100%)";
+      sections[i].style.zIndex = `1`;
+    }
+    sections[mainIndex - 1].style.transform = "translate(0px, -100%)";
+    // sections[mainIndex - 1].style.zIndex = `2`;
+  } 
+
+  sections[mainIndex].style.transform = `translate(0px, 0px)`;
+  sections[mainIndex].style.zIndex = `5`;
+}
+
+
+
+// scroll to move 휠 이동 변화하면 사라지기
+function hiddenScrollToMove() {
+  scrollToMoveAni.classList.add('hidden');
+}
+
+
+function pagination(idx) {
+  const paginationDiv = document.querySelector('.pagination');
+  paginationDiv.innerHTML = `4 | <span>${idx + 1}</span>`;
+}
+
+
+
+// ASIDE BTN 
 const upBtn = document.querySelector('.up_btn');
 const downBtn = document.querySelector('.down_btn');
 
-upBtn.addEventListener('click', () => goToMain(-1));
-downBtn.addEventListener('click', () => goToMain(1));
+upBtn.addEventListener('click', prevMain);
+downBtn.addEventListener('click', nextMain);
 
 
 
-// 2초마다 휠값을 판별
-let goToMainNextLoop = setInterval(onWheelEvent, 2000);
+// n초마다 휠값을 판별
+let goToMainNextLoop = setInterval(onWheelEvent, 1700);
 
 // 휠 작동시 이동은 한번만 작동
 function onWheelEvent() {
   document.addEventListener('wheel', e => {
     // console.log(e);
     if (e.deltaY > 0) {
+      nextMain();
       console.log("down");
-      goToMain(1);
     } else if (e.deltaY < 0) {
-      goToMain(-1)
+      prevMain()
       console.log("up");
     }
+    hiddenScrollToMove();
   }, {once: true});
 }
-
-
-
 
 
 
