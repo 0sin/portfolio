683c61a65688b3c14ca24c706930da2c1cf88009
0sin <69961808+0sin@users.noreply.github.com>
2021-02-11 23:15:09 +0900

    Update main

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-'use strick';
+'use strict';
 
 // MOUSE EVENT
 let mousePointer;
@@ -6,38 +6,63 @@ let x = 0;
 let y = 0;
 let mx = 0;
 let my = 0;
-let speed = 0.12;
+const SPEED = 0.12;
+
+function loop() {
+  mx += (x - mx) * SPEED;
+  my += (y - my) * SPEED;
+  mousePointer.style.transform = `translate(${mx}px, ${my}px)`;
+  window.requestAnimationFrame(loop);
+}
 
 window.onload = function() {
   mousePointer = document.querySelector('.mouse_pointer');
 
-  window.addEventListener('mousemove', mouseFunc, false);
-  
   function mouseFunc(e) {
     x = e.clientX;
     y = e.clientY;
   }
 
+  window.addEventListener('mousemove', mouseFunc, false);
+
   loop();
 }
 
-function loop() {
-  mx += (x - mx) * speed;
-  my += (y - my) * speed;
-  mousePointer.style.transform = `translate(${mx}px, ${my}px)`;
-  window.requestAnimationFrame(loop);
-}
-
+const stopMoveMousePointer = undefined;
 
 
 
 // MAIN SECTION SLIDE
+// Variable Declare
 const sections = document.querySelectorAll('.main_section');
-let mainIndex = -1;
+const divBlack = document.querySelector('.div_black');
+const upBtn = document.querySelector('.up_btn');
+const downBtn = document.querySelector('.down_btn');
 
-const scrollToMoveAni = document.querySelector('.scroll_to_move');
+let mainIndex = 0;
+let goToMainNextLoop;
 
-function changeMainIdx(idx) {
+
+// Black Div  - Animation
+const stopAnimationBlackDiv = () => {
+  divBlack.style.animation = '';
+}
+const animationBlackDiv = function() {
+  divBlack.style.animation = 'showBlackDiv 1.3s ease-in';
+  setTimeout(stopAnimationBlackDiv, 1500);
+}
+
+
+
+// Pagination
+const pagination = (idx) => {
+  const paginationSpan = document.querySelector('.pagination span');
+  paginationSpan.innerText = idx + 1;
+}
+
+
+// Change Index
+const changeMainIdx = (idx) => {
   mainIndex += idx;
 
   if (mainIndex > sections.length - 1) {
@@ -49,104 +74,43 @@ function changeMainIdx(idx) {
   pagination(mainIndex);
 }
 
-nextMain(); //첫화면 호출
+const animationSection = () => {
+  setTimeout(() => {
+    sections.forEach(e => e.style.transform = 'translateX(100%)');
+    sections[mainIndex].style.transform = 'translateX(0px)';
+  }, 700)
+}
 
-function nextMain() {
+// Next Slide
+const nextSlide = () => {
   changeMainIdx(1);
-
-  for (let i=0; i<sections.length; i++) {
-    sections[i].style.transform = "translate(0px, 100%)";
-    sections[i].style.zIndex = `1`;
-  }
-
-  if (mainIndex === sections.length - 1) { //마지막 페이지 예외처리
-    for (let i=0; i<sections.length; i++) {
-      sections[i].style.transform = "translate(0px, -100%)";
-      sections[i].style.zIndex = `1`;
-    }
-    sections[0].style.transform = "translate(0px, 100%)";
-    sections[0].style.zIndex = `2`;
-  } else if (mainIndex === 0) { //마지막 페이지 예외처리
-    sections[sections.length - 1].style.transform = `translate(0px, -100%)`;
-    sections[sections.length - 1].style.zIndex = `2`;
-  } else { //중간 페이지 적용
-    sections[mainIndex - 1].style.transform = "translate(0px, -100%)";
-    sections[mainIndex - 1].style.zIndex = `2`;
-  } 
-
-  sections[mainIndex].style.transform = `translate(0px, 0px)`;
-  sections[mainIndex].style.zIndex = `5`;
-
+  animationBlackDiv();
+  animationSection();
 }
 
-
-function prevMain() {
+// Prev Slide
+const prevSlide = () => {
   changeMainIdx(-1);
-
-  if (mainIndex === 0) {
-    for (let i=0; i<sections.length; i++) {
-      sections[i].style.transform = "translate(0px, 100%)";
-      sections[i].style.zIndex = `2`;
-    }
-    sections[sections.length - 1].style.transform = `translate(0px, -100%)`;
-    sections[sections.length - 1].style.zIndex = `1`;
-  } else if (mainIndex === sections.length - 1) {
-    for (let i=0; i<sections.length; i++) {
-      sections[i].style.transform = "translate(0px, -100%)";
-      sections[i].style.zIndex = `1`;
-    }
-    sections[0].style.transform = "translate(0px, 100%)";
-    sections[0].style.zIndex = `2`;
-  } else {
-    for (let i=0; i<sections.length; i++) {
-      sections[i].style.transform = "translate(0px, 100%)";
-      sections[i].style.zIndex = `1`;
-    }
-    sections[mainIndex - 1].style.transform = "translate(0px, -100%)";
-    // sections[mainIndex - 1].style.zIndex = `2`;
-  } 
-
-  sections[mainIndex].style.transform = `translate(0px, 0px)`;
-  sections[mainIndex].style.zIndex = `5`;
+  animationBlackDiv();
+  animationSection();
 }
 
 
-
-// scroll to move 휠 이동 변화하면 사라지기
-function hiddenScrollToMove() {
+// scroll to move 휠 이동 감지하면 사라지기
+const hiddenScrollToMove = () => {
+  const scrollToMoveAni = document.querySelector('.scroll_to_move');
   scrollToMoveAni.classList.add('hidden');
 }
 
-
-function pagination(idx) {
-  const paginationDiv = document.querySelector('.pagination');
-  paginationDiv.innerHTML = `4 | <span>${idx + 1}</span>`;
-}
-
-
-
-// ASIDE BTN 
-const upBtn = document.querySelector('.up_btn');
-const downBtn = document.querySelector('.down_btn');
-
-upBtn.addEventListener('click', prevMain);
-downBtn.addEventListener('click', nextMain);
-
-
-
-// n초마다 휠값을 판별
-let goToMainNextLoop = setInterval(onWheelEvent, 1700);
-
 // 휠 작동시 이동은 한번만 작동
-function onWheelEvent() {
+const onWheelEvent = () => {
   document.addEventListener('wheel', e => {
-    // console.log(e);
     if (e.deltaY > 0) {
-      nextMain();
-      console.log("down");
+      nextSlide();
+      console.log('down');
     } else if (e.deltaY < 0) {
-      prevMain()
-      console.log("up");
+      prevSlide()
+      console.log('up');
     }
     hiddenScrollToMove();
   }, {once: true});
@@ -156,6 +120,9 @@ function onWheelEvent() {
 
 
 
+// CLICK ASIDE BTN 
+upBtn.addEventListener('click', prevSlide);
+downBtn.addEventListener('click', nextSlide);
 
 
 
@@ -181,15 +148,49 @@ const closeBar = document.querySelector('.close_bar');
 
 menuBar.addEventListener('click', function() {
   const sideNav = document.querySelector('.sidenav');
-  sideNav.style.width = "100vw";
-  sideNav.style.right = "0";
-  closeBar.style.opacity = "1";
-  closeBar.style.display = "block";
+  sideNav.style.width = '100vw';
+  sideNav.style.right = '0';
+  closeBar.style.opacity = '1';
+  closeBar.style.display = 'block';
 });
 closeBar.addEventListener('click', function() {
   const sideNav = document.querySelector('.sidenav');
-  sideNav.style.width = "0%";
-  sideNav.style.right = "-50%";
-  closeBar.style.opacity = "0";
-  closeBar.style.display = "none";
+  sideNav.style.width = '0%';
+  sideNav.style.right = '-50%';
+  closeBar.style.opacity = '0';
+  closeBar.style.display = 'none';
+});
+
+
+
+// 첫화면
+// 2초마다 휠값을 판별
+goToMainNextLoop = setInterval(onWheelEvent, 2000);
+
+
+
+
+// viewport 768px 이하 main static
+function tabletSize() {
+  if(window.matchMedia('(max-width: 768px)').matches) {
+    console.log('768이하')
+    stopAnimationBlackDiv();
+    clearInterval(goToMainNextLoop);
+  } else {
+    console.log('768이상')
+  }
+}
+tabletSize();
+
+
+
+// Resize
+window.addEventListener('resize', e => {
+  let width = e.target.innerWidth
+
+  if (width < 768) {
+    stopAnimationBlackDiv();
+    clearInterval(goToMainNextLoop);
+  }
+  console.log(width)
 });
